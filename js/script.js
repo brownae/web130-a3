45bcf44d4107af10acb296e8d1b134ba2a4d766d
brownae <aebrown9@gmail.com>
2017-05-05 07:17:07 -0700

    Cleans up indentation

--- js/script.js
+++ js/script.js
@@ -3,30 +3,27 @@ $(function() { // DOM Ready
 
     $('nav ul li > a:not(:only-child)').click(function(e) {
         if ($(document).width() >= 480 && $(document).width() <= 900){
-            //alert('Hello');
-            //$(this).siblings('.nav-dropdown').toggle().addClass('tablet-nav');
             $('.nav-dropdown').toggleClass('tablet-nav');
         }else
             $(this).siblings('.nav-dropdown').toggle();
-            //$(this).siblings('.nav-dropdown').toggle();
 
     //Prevent other nav-dropdowns from opening when one is clicked
     $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
+        e.stopPropagation();
     });
 
     //this makes the menu hide again if someone clicks outside of the nav(ie on the html)
     $('html').click(function() {
-      $('.nav-dropdown').hide();
+        $('.nav-dropdown').hide();
     });
 
     //toggles the mobile X and hamburger
     $('#nav-toggle').on('click', function() {
-    this.classList.toggle('active');
+        this.classList.toggle('active');
     });
 
     $('#nav-toggle').click(function() {
-    $('nav ul').toggle();
+        $('nav ul').toggle();
     });
 
 });
